b0ba275d3ac4f0ad8e7807d1c9df82aed9e43377
welker arantes <elkerafereira@hotmail.com>
2014-11-19 16:23:26 -0200

    commit de atualização da pagina inical

--- home.php
+++ home.php
@@ -15,6 +15,20 @@
   <div class="row" id="main-content">
   <div class="well" align="center">
   	<br>jdopajdfaj<br>
+
+   <section class="carousel">
+   	<div class="carousel-inner">
+
+   		<figure class="item">
+   			<img src="..model/img/Ryu2.jpg" alt="">
+   			<figurecaption class="carousel-caption">
+   				<h4>adjfadjfadjf</h4>
+   				<p>dfadjfakjdf</p>
+   			</figurecaption>
+   		</figure>
+   	</div>
+   </section>
+
   </div>	
   </div>
   </div>
